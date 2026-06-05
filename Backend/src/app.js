561e75ee3aa7c76913b7f4fd89786c6648700635
Vandan-dabhi <vandandabhi287@gmail.com>
2026-06-05 13:56:34 +0530

    add origin of vercel in backend cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,8 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
+  origin: [ "http://localhost:5173",
+          "https://job-portal-seven-neon.vercel.app/"
+  ],
+       credentials: true
 }));
 
 
